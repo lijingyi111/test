f3547cfac305cbab4cbb2766a55ed2dcef63d836
user <user@192.168.0.106>
2019-10-24 15:04:33 +0900

    追加

--- src/dcnet/DCNET.java
+++ src/dcnet/DCNET.java
@@ -17,6 +17,7 @@ public class DCNET {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("object");
+        System.out.println("test");
     }
     
 }
